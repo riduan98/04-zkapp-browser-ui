3a33459442f06604ede2ab52e25d5ab29b7ed85d
riduan98 <franz.kafka0001@gmail.com>
2022-12-14 01:01:28 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-6b89986674debd0d.js
+++ _next/static/chunks/webpack-6b89986674debd0d.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "be9c877b44c2b386" + ".css";
+/******/ 			return "static/css/" + "866df1e16c543bea" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
